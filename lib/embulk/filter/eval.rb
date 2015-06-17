30025824cb99ae1c9743e49d5f14dfaececd39e0
Hironori Ogibayashi <hironori.ogibayashi@linecorp.com>
2015-06-17 15:09:49 +0900

    Added add_columns parameters to add columns.
    example configuration:
    
    ------
    filters:
      - type: eval
        add_columns:
          - {name: file_method_line, type: string, value: '[record["filename"],record["method"],record["line"].to_s].join("_")' }
    ------

--- lib/embulk/filter/eval.rb
+++ lib/embulk/filter/eval.rb
@@ -4,22 +4,23 @@ module Embulk
     class EvalFilterPlugin < FilterPlugin
       Plugin.register_filter("eval", self)
 
-      class NotFoundOutColumn < StandardError; end;
+      class NotFoundOutColumn < StandardError; end
       VERSION = '0.1.0'
 
       def self.transaction(config, in_schema, &control)
         # configuration code:
         task = {
           "eval_columns" => config.param("eval_columns", :array, default: []),
-          "out_columns" => config.param("out_columns", :array, default: [])
+          "out_columns" => config.param("out_columns", :array, default: []),
+          "add_columns" => config.param("add_columns", :array, default: []),
         }
 
-        out_schema = out_schema(task['out_columns'], in_schema)
+        out_schema = out_schema(task['out_columns'], in_schema, task['add_columns'])
 
         yield(task, out_schema)
       end
 
-      def self.out_schema(out_columns, in_schema)
+      def self.out_schema(out_columns, in_schema, add_columns)
         schema = out_columns.map.with_index do |name, i|
           sch = in_schema.find { |sch| sch.name == name }
 
@@ -29,11 +30,17 @@ module Embulk
 
           Embulk::Column.new(index: i, name: sch.name, type: sch.type, format: sch.format)
         end
-        schema.empty? ? in_schema : schema
+        out_schema = schema.empty? ? in_schema : schema
+        base_index = out_schema.size
+        add_schema = add_columns.map.with_index do |v, i|
+          Embulk::Column.new(index: base_index + i, name: v['name'], type: v['type'].to_sym)
+        end
+        out_schema + add_schema
       end
 
       def init
         @table = task["eval_columns"]
+        @add_table = task["add_columns"]
       end
 
       def close
@@ -57,7 +64,11 @@ module Embulk
                 result[key] = value
               end
             end
-
+            
+            @add_table.each do |item|
+              result[item["name"]] = type_convert(eval(item["value"]),item["type"],item["opts"])
+            end
+            
             page_builder.add(result.values)
           rescue
           end
@@ -71,7 +82,24 @@ module Embulk
       def hash_record(record)
         Hash[in_schema.names.zip(record)]
       end
-    end
 
+      def type_convert(v, field_type,opts={  })
+        case field_type
+          when "string"
+            v
+          when "long"
+            v.to_i
+          when "double"
+            v.to_f
+          when "timestamp"
+            DateTime.strptime(v, opts["time_format"]).to_time
+          when "boolean"
+            BOOLEAN_TYPES.include? v.downcase
+          else
+            raise "unsupported type #{field_type}"
+        end
+      end
+
+    end
   end
 end
